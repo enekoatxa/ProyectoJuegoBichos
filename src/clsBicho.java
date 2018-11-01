3fbc3e0b8d083dbfc8c746668fb87458e766418d
ALUMNO <mikelmartinezz@opendeusto.es>
2018-11-01 12:48:14 +0100

    mikel

--- src/clsBicho.java
+++ src/clsBicho.java
@@ -1,16 +1,51 @@
+import java.awt.MouseInfo;
+
+import com.sun.javafx.scene.paint.GradientUtils.Point;
+
 
 public class clsBicho 
 {
 	private double posX;
 	private double posY;
-	private final int circle = 15;
 	private lblBicho label; 
+	// tipo bicho elegitzeko aukera ere hemendikan?
+	
 	
 	public clsBicho(double posX, double posY)
 	{
+		
 		this.posX=posX;
 		this.posY=posY;
 		label = new lblBicho();
+		
 	}
+	
+	public double getPosX() {
+		return posX;
+	}
+
+	public void setPosX(double posX) {
+		this.posX = posX;
+	}
+
+	public double getPosY() {
+		return posY;
+	}
+
+	public void setPosY(double posY) {
+		this.posY = posY;
+	}
+
+	public lblBicho getLabel() {
+		return label;
+	}
+
+	public void setLabel(lblBicho label) {
+		this.label = label;
+	}
+
+	
+	
+	
 }
 
--- src/vtPrincipal.java
+++ src/vtPrincipal.java
@@ -1,6 +1,36 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 public class vtPrincipal extends JFrame
 {
+	private JPanel contentPane;
+
+	//main hau probisionala dek
+	
+	public static void main(String[] args) {
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					vtPrincipal frame = new vtPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	public vtPrincipal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
 
 }
