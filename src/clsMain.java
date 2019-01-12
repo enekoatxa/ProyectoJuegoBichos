1c508bb2101c57b091979a9338429ec5404a15ed
ALUMNO <mikelmartinezz@opendeusto.es>
2019-01-12 21:54:36 +0100

    prepull

--- src/clsMain.java
+++ src/clsMain.java
@@ -8,6 +8,7 @@ public class clsMain
 		EventQueue.invokeLater(new Runnable() {
 				public void run() {
 					try {
+						
 						vtPrincipal frame = new vtPrincipal(null);
 						frame.setVisible(true);
 						
